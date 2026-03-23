dbc8e830b53bf3f5e855b83f2342285e4637b9e4
Wiam_BABIOUI <wiam.babioui@teknologiate.com>
2026-03-23 19:22:04 +0100

    Prepare backend for deployment

--- talibJobFrontend/src/services/api.jsx
+++ talibJobFrontend/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'https://talibjob-backend.up.railway.app/api', // URL Railway
   headers: { 'Content-Type': 'application/json' },
 });
 
