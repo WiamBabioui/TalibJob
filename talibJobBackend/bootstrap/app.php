651638bbee927cf858b820a2e243f8f8b657185f
Wiam_BABIOUI <wiam.babioui@teknologiate.com>
2026-03-25 22:15:19 +0100

    Fix: move MissionController and simplify bootstrap

--- talibJobBackend/bootstrap/app.php
+++ talibJobBackend/bootstrap/app.php
@@ -7,7 +7,6 @@ use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        // On ne garde que l'API si web.php n'existe pas
         api: __DIR__.'/../routes/api.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
@@ -20,9 +19,5 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        $exceptions->render(function (\Illuminate\Auth\AuthenticationException $e, Request $request) {
-            if ($request->is('api/*')) {
-                return response()->json(['error' => 'Non authentifié.'], 401);
-            }
-        });
+        //
     })->create();
