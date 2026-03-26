16771c6bc8a068e8b4a2017176cab7a450ec4978
Wiam_BABIOUI <wiam.babioui@teknologiate.com>
2026-03-26 10:50:38 +0100

    remove cached config and secrets

--- talibJobFrontend/src/services/api.jsx
+++ talibJobFrontend/src/services/api.jsx
@@ -1,10 +1,50 @@
 import axios from "axios";
 
-const api = axios.create({
+const API = axios.create({
   baseURL: "https://talibjob-backend.up.railway.app/api",
   headers: {
-    "Content-Type": "application/json"
-  }
+    "Content-Type": "application/json",
+  },
 });
 
-export default api;
+// Ajouter automatiquement le token si connecté
+API.interceptors.request.use((config) => {
+
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+// -------------------- ETUDIANT --------------------
+
+export const registerStudent = (data) => {
+  return API.post("/etudiant/register", data);
+};
+
+export const loginStudent = (data) => {
+  return API.post("/etudiant/login", data);
+};
+
+export const getStudentProfile = () => {
+  return API.get("/etudiant/me");
+};
+
+export const logoutStudent = () => {
+  return API.post("/etudiant/logout");
+};
+
+// -------------------- MISSIONS --------------------
+
+export const getMissions = () => {
+  return API.get("/missions");
+};
+
+export const getMission = (id) => {
+  return API.get(`/missions/${id}`);
+};
+
+export default API;
